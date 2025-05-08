f7109cb20d7e9cd5c678f0ade1186bc1d7f1c2f4
VerkhovtsovDenis <verkhovtcov.do@dvfu.ru>
2025-05-09 02:23:58 +1000

    Приложения добавлены к проекту для определения миграций

--- system/mysite/settings.py
+++ system/mysite/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'CogEditor.apps.CogeditorConfig',
+    'CogSolver.apps.CogsolverConfig',
+    'CogNeural.apps.CogneuralConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -86,19 +89,19 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.' /
+        'NAME': 'django.contrib.auth.password_validation.'
                 'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.' /
+        'NAME': 'django.contrib.auth.password_validation.'
                 'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.' /
+        'NAME': 'django.contrib.auth.password_validation.'
                 'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.' /
+        'NAME': 'django.contrib.auth.password_validation.'
                 'NumericPasswordValidator',
     },
 ]
