40fbc30161d4fe801b5d8ed24b6d0e35fe61ca8f
VerkhovtsovDenis <verkhovtcov.do@dvfu.ru>
2025-05-09 05:23:22 +1000

    Добавлена локализация наименования приложения

--- system/CogEditor/apps.py
+++ system/CogEditor/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class CogeditorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'CogEditor'
+    verbose_name = 'Редактор знаний'
