662e32374d3701947068623a36ff0da19c3675c7
VerkhovtsovDenis <verhovtsov.denis@mail.ru>
2025-05-09 19:36:04 +1000

    Добавлена бизнес-логика отображения данных

--- system/CogSolver/views.py
+++ system/CogSolver/views.py
@@ -1,5 +1,18 @@
-from django.http import HttpResponse
+from CogSolver.models import RuleEngine
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    results = RuleEngine.batch_apply_rules()
+    context = {
+        'results': results,
+        'columns': [
+            {'name': 'ID', 'key': 'application.id'},
+            {'name': 'Название', 'key': 'application.e_title'},
+            {'name': 'Текущий статус', 'key': 'current_status'},
+            {'name': 'Новый статус', 'key': 'new_status'},
+            {'name': 'Изменен?', 'key': 'status_changed'},
+        ]
+    }
+    return render(request, 'CogSolver/index.html', context)
+
