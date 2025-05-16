59ba2e951a7e6b282ebe472451b5c00efda58a90
VerkhovtsovDenis <verhovtsov.denis@mail.ru>
2025-05-17 05:14:41 +1000

    Добавлены header и footer страниц
    	modified:   system/CogEditor/urls.py
    	modified:   system/CogNeural/urls.py
    	modified:   system/CogSolver/urls.py
    	modified:   system/mysite/settings.py
    	new file:   system/static/img/ru.png
    	modified:   system/templates/base.html
    	new file:   system/templates/includes/footer.html
    	new file:   system/templates/includes/header.html

--- system/CogEditor/urls.py
+++ system/CogEditor/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "CogEditor"
+
+
 urlpatterns = [
     path("",
          views.index,
--- system/CogSolver/urls.py
+++ system/CogSolver/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "CogSolver"
+
 urlpatterns = [
     path("", views.rules_report, name="rules_report"),
 ]
