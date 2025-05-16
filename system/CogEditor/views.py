72e8ea93c26aea68623649b66a78083eb1f0d28d
VerkhovtsovDenis <verhovtsov.denis@mail.ru>
2025-05-17 06:08:45 +1000

    Обновлена логика отображения заявок организатора

--- system/CogEditor/views.py
+++ system/CogEditor/views.py
@@ -1,6 +1,7 @@
 from CogEditor.models import Application, Employee
 from django.shortcuts import get_object_or_404
 from django.views import generic
+from CogEditor.models import StructuralUnit
 
 
 class IndexView(generic.ListView):
@@ -17,47 +18,69 @@ class DetailView(generic.DetailView):
     template_name = "CogEditor/detail.html"
 
 
-class ArchiveByYearMonthView(generic.ListView):
-    template_name = "CogEditor/archive_list.html"
-    context_object_name = "application_list"
-
-    def get_queryset(self):
-        year = self.kwargs["year"]
-        month = self.kwargs["month"]
-
-        # Получаем заявки, у которых есть хотя бы одно событие
-        # в указанном году и месяце
-        return (
-            Application.objects.filter(
-                event_schedule__start__year=year,
-                event_schedule__start__month=month,
-            )
-            .distinct()
-            .order_by("subm_date")
-        )
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["year"] = self.kwargs["year"]
-        context["month"] = self.kwargs["month"]
-        return context
-
-
 class ArchiveByYearView(generic.ListView):
     template_name = "CogEditor/archive_list.html"
     context_object_name = "application_list"
+    paginate_by = 20
 
     def get_queryset(self):
         year = self.kwargs["year"]
         return (
             Application.objects.filter(event_schedule__start__year=year)
             .distinct()
-            .order_by("subm_date")
+            .order_by("-subm_date")
+            .prefetch_related("event_schedule")
         )
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["year"] = self.kwargs["year"]
+        context["archive_type"] = "year"
+        return context
+
+
+class ArchiveByYearMonthView(generic.ListView):
+    template_name = "CogEditor/archive_list.html"
+    context_object_name = "application_list"
+    paginate_by = 20
+
+    def get_queryset(self):
+        year = self.kwargs["year"]
+        month = self.kwargs["month"]
+        return (
+            Application.objects.filter(
+                event_schedule__start__year=year,
+                event_schedule__start__month=month,
+            )
+            .distinct()
+            .order_by("-subm_date")
+            .prefetch_related("event_schedule")
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["year"] = self.kwargs["year"]
+        context["month"] = self.kwargs["month"]
+        context["archive_type"] = "month"
+        return context
+
+
+class OrganizerEventsView(generic.ListView):
+    template_name = "CogEditor/organizer_events.html"
+    context_object_name = "application_list"
+    paginate_by = 20
+
+    def get_queryset(self):
+        organizer_id = self.kwargs["id"]
+        return (
+            Application.objects.filter(organizer_id=organizer_id)
+            .order_by("-subm_date")
+            .prefetch_related("event_schedule")
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["organizer"] = StructuralUnit.objects.get(pk=self.kwargs["id"])
         return context
 
 
@@ -66,21 +89,3 @@ class FullArchiveView(generic.ListView):
     context_object_name = "application_list"
     queryset = Application.objects.all().order_by("-subm_date")
     paginate_by = 20
-
-
-class OrganizerEventsView(generic.ListView):
-    template_name = "CogEditor/organizer_events.html"
-    context_object_name = "application_list"
-
-    def get_queryset(self):
-        organizer = get_object_or_404(Employee, pk=self.kwargs["id"])
-        return Application.objects.filter(organizer=organizer).order_by(
-            "-subm_date"
-        )
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["organizer"] = get_object_or_404(
-            Employee, pk=self.kwargs["id"]
-        )
-        return context
