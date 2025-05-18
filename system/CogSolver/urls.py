c82339aa925e9b3f77d6bc765377960672157ed6
VerkhovtsovDenis <verhovtsov.denis@mail.ru>
2025-05-19 04:43:05 +1000

    Рефакторинг кода - приведение стилей в соответствие flake8

--- system/CogSolver/urls.py
+++ system/CogSolver/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = "CogSolver"
 
 urlpatterns = [
-    path("application_classifier/", views.application_classifier, name="application_classifier"),
+    path("application_classifier/",
+         views.application_classifier,
+         name="application_classifier"),
     path("", views.rules_report, name="rules_report"),
 ]
