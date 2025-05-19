b2d762d993cfec32adcb69b858a621208c518cf9
VerkhovtsovDenis <verhovtsov.denis@mail.ru>
2025-05-20 01:27:03 +1000

    Добавлен тест для проверки валидации формы при корректных данных

--- system/CogEditor/tests/test_forms.py
+++ system/CogEditor/tests/test_forms.py
@@ -1,3 +1,6 @@
+import datetime
+
+from CogEditor.forms import ApplicationForm
 from CogEditor.models import (
     AgreedStatus,
     Employee,
@@ -7,6 +10,7 @@ from CogEditor.models import (
     StructuralUnit,
 )
 from django.test import TestCase
+from django.utils import timezone
 
 
 class ApplicationFormTest(TestCase):
@@ -21,3 +25,19 @@ class ApplicationFormTest(TestCase):
             full_name="Тест", position=self.position, structural_unit=self.unit
         )
         self.event_format = EventFormat.objects.create(name="Тест")
+
+    def test_valid_form(self):
+        tomorrow = timezone.now() + datetime.timedelta(days=1)
+        form_data = {
+            'e_title': 'Тестовое мероприятие',
+            'e_description': 'Тестовое описание',
+            'organizer_employee_name': 'Тестовый сотрудник',
+            'event_date': tomorrow.date(),
+            'event_time_start': datetime.time(10, 0),
+            'event_time_end': datetime.time(12, 0),
+            'organizer': self.unit.id,
+            'e_format': self.event_format.id,
+            'number_of_participants': 10,
+        }
+        form = ApplicationForm(data=form_data)
+        self.assertTrue(form.is_valid())
