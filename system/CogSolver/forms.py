39dcdd10eb928e1fe0f41183261ab11ec089a3ae
VerkhovtsovDenis <verhovtsov.denis@mail.ru>
2025-05-19 10:03:34 +1000

    Добавлены поля формы для корректного формирования дат мероприятия

--- system/CogSolver/forms.py
+++ system/CogSolver/forms.py
@@ -1,15 +1,22 @@
 from bootstrap_datepicker_plus.widgets import DatePickerInput
 from CogEditor.models import (AgreedStatus, Application, Employee,
-                              EmployeePosition, Sources)
+                              EmployeePosition, Sources, Schedule)
 from django import forms
 from django.utils import timezone
+import datetime
 
 
 class ApplicationForm(forms.ModelForm):
     e_title = forms.CharField(
         max_length=256,
-        widget=forms.Textarea(attrs={'rows': 2}),
+        widget=forms.Textarea(attrs={'rows': 1}),
         label="Наименование мероприятия*"
+    )
+
+    e_description = forms.CharField(
+        max_length=2048,
+        widget=forms.Textarea(attrs={'rows': 8}),
+        label="Описание мероприятия",
     )
 
     organizer_employee_name = forms.CharField(
@@ -18,9 +25,83 @@ class ApplicationForm(forms.ModelForm):
         required=True
     )
 
+    now = timezone.now()
+    local_now = timezone.localtime(now)
+    base_time = local_now.replace(minute=0, second=0, microsecond=0)
+    day_1 = base_time + datetime.timedelta(days=3, hours=1)
+    day_2 = base_time + datetime.timedelta(days=3, hours=4)
+
+    event_date = forms.DateField(
+        label="Дата мероприятия",
+        widget=forms.DateInput(attrs={
+            'class': 'form-control form-control-sm',
+            'type': 'date',
+            'value': day_1.date().strftime("%Y-%m-%d")
+        }),
+        initial=day_1.date().strftime("%Y-%m-%d")
+    )
+    # FIXME
+    event_date_end = forms.DateField(
+        label="Дата окончания",
+        widget=forms.DateInput(attrs={
+            'class': 'form-control form-control-sm',
+            'type': 'date',
+        }),
+        required=False,
+        initial=day_2.date().strftime("%Y-%m-%d")
+    )
+
+    event_time_start = forms.TimeField(
+        label="Время начала",
+        widget=forms.TimeInput(attrs={
+            'class': 'form-control form-control-sm',
+            'type': 'time',
+            'value': day_1.time(),
+            'format': '%HH:%MM',
+        }),
+        initial=day_1.time(),
+        input_formats=['%HH:%MM'],
+    )
+
+    event_time_end = forms.TimeField(
+        label="Время окончания",
+        widget=forms.TimeInput(attrs={
+            'class': 'form-control form-control-sm',
+            'type': 'time',
+        }),
+        initial=day_2.time(),
+        required=False,
+        input_formats=['%HH:%MM'],
+    )
+
+    event_all_day = forms.BooleanField(
+        label="Весь день",
+        required=False,
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'})
+    )
+
+    # Поля для монтажа/демонтажа
+    setup_date_start = forms.DateField(
+        label="Дата начала монтажа",
+        widget=forms.DateInput(attrs={
+            'class': 'form-control form-control-sm',
+            'type': 'date'
+        }),
+        required=False
+    )
+
+    setup_date_end = forms.DateField(
+        label="Дата окончания демонтажа",
+        widget=forms.DateInput(attrs={
+            'class': 'form-control form-control-sm',
+            'type': 'date'
+        }),
+        required=False
+    )
+
     class Meta:
         model = Application
-        exclude = ['application_source', 'organizer_employee']
+        exclude = ['application_source', 'organizer_employee', 'event_schedule', 'installation_deinstallation']
         widgets = {
             'subm_date': forms.HiddenInput(),  # Скрытое поле
             'status': forms.HiddenInput(),     # Скрытое поле
@@ -48,7 +129,40 @@ class ApplicationForm(forms.ModelForm):
 
     def clean(self):
         cleaned_data = super().clean()
+        all_day = cleaned_data.get('event_all_day')
 
+        if all_day:
+            date_start = cleaned_data.get('event_date_start')
+            date_end = cleaned_data.get('event_date_end')
+            
+            if not date_start or not date_end:
+                raise forms.ValidationError("Для мероприятия на весь день необходимо указать даты начала и окончания")
+                
+            if date_start > date_end:
+                raise forms.ValidationError("Дата начала не может быть позже даты окончания")
+            
+            import datetime
+            cleaned_data['event_start'] = datetime.datetime.combine(date_start, datetime.time.min)
+            cleaned_data['event_end'] = datetime.datetime.combine(date_end, datetime.time.max)
+        else:
+            event_date = cleaned_data.get('event_date')
+            # FIXME time_end, time_start is none
+
+            time_start = cleaned_data.get('Event_Time_Start')
+            time_end = cleaned_data.get('Event_Time_End')
+            
+            if not event_date:
+                raise forms.ValidationError("Необходимо указать дату мероприятия")
+
+            if time_start is None or time_end is None:
+                raise forms.ValidationError("Необходимо указать время начала и окончания")
+
+            if time_start >= time_end:
+                raise forms.ValidationError("Время начала не может быть позже времени окончания")
+                
+            cleaned_data['event_start'] = datetime.datetime.combine(event_date, time_start)
+            cleaned_data['event_end'] = datetime.datetime.combine(event_date, time_end)
+            print(cleaned_data['event_start'], cleaned_data['event_end'])
         # Гарантируем установку обязательных полей
         if not cleaned_data.get('subm_date'):
             cleaned_data['subm_date'] = timezone.now()
@@ -70,6 +184,18 @@ class ApplicationForm(forms.ModelForm):
 
         if not instance.application_source_id:
             instance.application_source = Sources.objects.get(name="Сайт")
+
+        # Удаляем старые расписания
+        instance.event_schedule.all().delete()
+        
+        # Создаем новое расписание
+        schedule = Schedule.objects.create(
+            start=self.cleaned_data['event_start'],
+            end=self.cleaned_data['event_end'],
+            all_day=self.cleaned_data['event_all_day']
+        )
+        instance.event_schedule.add(schedule)
+
 
         # Обработка сотрудника
         full_name = self.cleaned_data['organizer_employee_name']
