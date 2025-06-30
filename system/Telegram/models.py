b1fe2b291fa828b69186468e89dda8c0705da62d
VerkhovtsovDenis <verkhovtcov.do@students.dvfu.ru>
2025-06-30 22:13:04 +1000

    Добавлена модель TelegramChat

--- system/Telegram/models.py
+++ system/Telegram/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class TelegramChat(models.Model):
+    phone = models.CharField(
+        verbose_name="Номер телефона",
+        blank=None,
+        null=None,
+    )
+
+    telegram_chat_id = models.CharField(
+        verbose_name="ID чата telegram",
+        blank=None,
+        null=None,
+    )
