a9324ae8edd17f61ffa3b6fd3fbc3e502e39621d
VerkhovtsovDenis <verkhovtcov.do@students.dvfu.ru>
2025-07-01 06:50:51 +1000

    Добавлен модуль Telegram

--- system/mysite/urls.py
+++ system/mysite/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", include("CogEditor.urls")),
     path("neural/", include("CogNeural.urls")),
     path("solver/", include("CogSolver.urls")),
+    path('telegram/', include("Telegram.urls")),
     path("admin/", admin.site.urls),
 ]
